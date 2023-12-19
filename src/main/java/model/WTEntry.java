57fa78c9790a85e5b6111ef25353ae8c7020703b
sproshut <92974238+sproshut@users.noreply.github.com>
2023-12-19 11:30:49 +0100

    Basic word entry structure

--- src/main/java/model/WTEntry.java
+++ src/main/java/model/WTEntry.java
@@ -1,12 +1,34 @@
 package model;
 
+import model.exceptions.InvalidURLException;
+import model.exceptions.InvalidWordException;
+
+import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class WTEntry {
     private String word;
     private String url;
 
     public WTEntry(String word, String url) {
-        this.word = word;
-        this.url = url;
+        setWord(word);
+        setURL(url);
+    }
+
+    private boolean checkURL(String url) {
+        if (url == null || url.isEmpty()) return false;
+        url = url.toLowerCase();
+        try {
+            new URL(url);
+        } catch (MalformedURLException e) {
+            return false;
+        }
+        return (url.endsWith(".jpg") || url.endsWith(".jpeg") || url.endsWith(".png"));
+    }
+
+    private boolean isWordInvalid(String word) {
+        return word == null || word.isEmpty();
     }
 
     public String getURL() {
@@ -14,6 +36,7 @@ public class WTEntry {
     }
 
     public void setURL(String url) {
+        if(checkURL(url)) throw new InvalidURLException("Invalid URL. Make sure not to use subdomains and add picture path (png, jpg, jpeg) at the end");
         this.url = url;
     }
 
@@ -22,6 +45,7 @@ public class WTEntry {
     }
 
     public void setWord(String word) {
+        if(isWordInvalid(word)) throw new InvalidWordException("Empty word entered");
         this.word = word;
     }
 }
